898c844e2225df1aaec1922f9b4fe0ed90a70765
RowanBevan <rb2180@bath.ac.uk>
2022-11-08 14:06:03 +0000

    One small step

--- q_learning.py
+++ q_learning.py
@@ -1,3 +1,25 @@
+import numpy as np
+
 class QLearning:
-    def __init__():
-        pass
+    def __init__(self, env, alpha, gamma, epsilon):
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        
+        self.Q = {}
+        for state in self.env.observation_space:
+            for action in self.env.action_space:
+                self.Q[(state, action)] = 0
+
+    def train(self):
+        for episode in range(1000):
+            S = self.env.reset()
+            total_reward = 0
+
+            # for _ in range():
+
+
+# https://github.com/lazavgeridis/LunarLander-v2/blob/main/rl_landers.py
+
+                    
