039685b8a2101e2c7dabfc82e69e7717a0041295
Max Wood <bridges.wood@gmail.com>
2022-11-08 15:44:00 +0000

    Fix keyboard_agent

--- keyboard_agent.py
+++ keyboard_agent.py
@@ -4,18 +4,19 @@ import gym
 import numpy as np
 from gym.utils.play import play
 
-def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
-    print(obs_t)
 
+# Open file
+f = open("data.txt", "w")
+
+def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
+    # Write to file
+    f.write(str(obs_t) + " " + str(obs_tp1) + " " + str(action) + " " + str(rew) + " " + str(terminated) + " " + str(truncated) + " " + str(info))
 
 # Wrap env in PlayableGame
-play(gym.make("CarRacing-v2", render_mode="rgb_array"), keys_to_action={
-"w": np.array([0, 0.7, 0]),
-"a": np.array([-1, 0, 0]),
-"s": np.array([0, 0, 1]),
-"d": np.array([1, 0, 0]),
-"wa": np.array([-1, 0.7, 0]),
-"dw": np.array([1, 0.7, 0]),
-"ds": np.array([1, 0, 1]),
-"as": np.array([-1, 0, 1]),
-}, noop=np.array([0,0,0]), callback=car_callback)
+play(gym.make("LunarLander-v2", render_mode="rgb_array", continuous=True), keys_to_action={
+"a": np.array([-1.0, -1.0]),
+"w": np.array([1.0, 0.0]),
+"d": np.array([-1.0, 1.0]),
+"wa": np.array([1.0, -1.0]),
+"wd": np.array([1.0, 1.0]),
+}, noop=np.array([0,0]), callback=car_callback)
