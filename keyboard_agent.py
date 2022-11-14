c2858517a3b06b3fe5d7f6a41bf08d27dca782f8
cam-arch <40884189+cam-arch@users.noreply.github.com>
2022-11-14 18:08:22 +0000

    Records Data and uploads to new JSON per episode

--- keyboard_agent.py
+++ keyboard_agent.py
@@ -3,14 +3,66 @@
 import gym
 import numpy as np
 from gym.utils.play import play
+import json
+import time
 
+stateDataToDump = []
 
-# Open file
-f = open("data.txt", "w")
+episodeData = {
+            "strategy": "example", # Fill in whatever the chosen strategy is, if none, put 'N/A'
+            "user": "Cam", # Fill in the user who is controlling this demonstration episode
+            "data": stateDataToDump
+    }
+
+def writeFile(jsonFile):
+    episodeDataToDump = json.dumps(episodeData, indent=4)
+    jsonFile.write(episodeDataToDump)
+
+    jsonFile.close()
+
+    
+jsonFile = open("epNo" + str(time.time()) + ".json", "w")
+
 
 def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
     # Write to file
-    f.write(str(obs_t) + " " + str(obs_tp1) + " " + str(action) + " " + str(rew) + " " + str(terminated) + " " + str(truncated) + " " + str(info))
+
+    if terminated == False:
+
+        print ("...")
+        print ("Timestep")
+
+
+        print (type(obs_tp1))
+    
+        print (obs_tp1)
+
+        print (obs_tp1.tolist())
+
+        stateData = {
+            "obs_t": obs_t[0].tolist(),
+            "obs_tp1": obs_tp1.tolist(),
+            "action": action.tolist(),
+            "rew": rew,
+            "terminated": terminated,
+            "truncated": truncated,
+            "info": info
+        }
+        
+        stateDataToDump.append(stateData)
+
+    else:
+
+        jsonFile = open("epNo" + str(time.time()) + ".json", "w")
+        
+        writeFile(jsonFile)
+
+        stateDataToDump.clear()
+
+
+
+        
+        
 
 # Wrap env in PlayableGame
 play(gym.make("LunarLander-v2", render_mode="rgb_array", continuous=True), keys_to_action={
@@ -20,3 +72,6 @@ play(gym.make("LunarLander-v2", render_mode="rgb_array", continuous=True), keys_
 "wa": np.array([1.0, -1.0]),
 "wd": np.array([1.0, 1.0]),
 }, noop=np.array([0,0]), callback=car_callback)
+
+
+
