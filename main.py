8690795e9b949461769e3b69f0074327898f9907
RowanBevan <rb2180@bath.ac.uk>
2022-11-14 18:08:29 +0000

    one smaller step

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import gym
 import numpy as np
 from gym.utils.play import play
+from q_learning import QLearning
 
 def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
     print(obs_t)
 
 
-env = gym.make("LunarLander-v2", render_mode="rgb_array")
-play(env, noop=np.array([0,0,0]), callback=car_callback)
+env = gym.make("LunarLander-v2", render_mode="human", continuous=False)
+#play(env, noop=np.array([0,0,0]), callback=car_callback)
+
+qlearn = QLearning(env)
+qlearn.train()
--- q_learning.py
+++ q_learning.py
@@ -1,5 +1,21 @@
 import numpy as np
 
+
+def discretize_state(s, ep):
+    state = s[0]
+    print(ep, state)
+    print(type(state))
+    discrete_state = (min(2, max(-2, int(state[0] / 0.05))), \
+                        min(2, max(-2, int(state[1] / 0.1))), \
+                        min(2, max(-2, int(state[2] / 0.1))), \
+                        min(2, max(-2, int(state[3] / 0.1))), \
+                        min(2, max(-2, int(state[4] / 0.1))), \
+                        min(2, max(-2, int(state[5] / 0.1))), \
+                        int(state[6]), \
+                        int(state[7]))
+
+    return discrete_state
+
 class QLearning:
     def __init__(self, env, alpha = 0.5, gamma = 0.9, epsilon = 1.0):
         self.env = env
@@ -8,52 +24,40 @@ class QLearning:
         self.epsilon = epsilon
 
         self.Q = {}
-        for state in self.env.observation_space:
-            for action in self.env.action_space:
-                self.Q[(state, action)] = 0
+
+       
 
     def train(self):
         for i in range(1000):
-            S = self.discretize_state(env.reset())
+            S = discretize_state(self.env.reset(), i)
+            #S = self.env.reset()
 
             while True:
-                env.render()
+                self.env.render()
 
                 # choose action A from state S using epsilon greedy policy
                 A = self.__epsilon_greedy(S)
 
                 # take action A, take immediate reward R and move to state S_
-                new_S, reward, terminated, _ = env.step(A)
-                S_ = discretize_state(new_S)
+                # S_, reward, terminated, _, _ = self.env.step(A)
+                new_S, reward, terminated, _, _ = self.env.step(A)
+                S_ = discretize_state(new_S, i)
 
                 # update Q values
                 if not terminated:
-                    x = gamma * self.__greedy(S_)
+                    x = self.gamma * self.__greedy(S_)
                     self.Q[(S, A)] += self.alpha * (reward + x - self.Q[(S, A)])
                 else:
                     self.Q[(S, A)] += self.alpha * (reward - self.Q[(S, A)])
-
-                if terminated:
                     break
 
                 S = S_
 
-    @staticmethod
-    def discretise_state(self, state):
-        return (min(2, max(-2, int((state[0]) / 0.05))),
-                min(2, max(-2, int((state[1]) / 0.1))),
-                min(2, max(-2, int((state[2]) / 0.1))),
-                min(2, max(-2, int((state[3]) / 0.1))),
-                min(2, max(-2, int((state[4]) / 0.1))),
-                min(2, max(-2, int((state[5]) / 0.1))),
-                               int(state[6]),
-                               int(state[7]))
-
     def __greedy(self, state):
-        return np.argmax([self.Q[(state, action)] for action in self.env.action_space])
+        return np.argmax([self.Q[(state, action)] for action in range(4)])
 
     def __epsilon_greedy(self, state):
         if np.random.random() < self.epsilon:
-            return random.choice(range(self.env.action_space.n))
+            return np.random.choice(range(self.env.action_space.n))
         else:
             return self.__greedy(state)
