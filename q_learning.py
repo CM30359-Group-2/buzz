57ed38cbd70aa45eb69412fb4f251772b174b83a
RowanBevan <rb2180@bath.ac.uk>
2022-12-01 14:54:18 +0000

    Bit of tinkering with variables

--- q_learning.py
+++ q_learning.py
@@ -3,20 +3,16 @@ import time
 import gym
 import json
 
-
-
 def discretize_state(s):
     state_ = s
-    discrete_state = (min(2, max(-2, int(state_[0] / 0.05))), \
-                        min(2, max(-2, int(state_[1] / 0.1))), \
-                        min(2, max(-2, int(state_[2] / 0.1))), \
-                        min(2, max(-2, int(state_[3] / 0.1))), \
-                        min(2, max(-2, int(state_[4]/ 0.1))), \
-                        min(2, max(-2, int(state_[5] / 0.1))), \
+    discrete_state = (min(3, max(-3, int(state_[0] / 0.033))), \
+                        min(2, max(-2, int(state_[1] / 0.125))), \
+                        min(2, max(-2, int(state_[2] / 0.125))), \
+                        min(2, max(-2, int(state_[3] / 0.125))), \
+                        min(2, max(-2, int(state_[4]/ 0.125))), \
+                        min(2, max(-2, int(state_[5] / 0.125))), \
                         int(state_[6]), \
                         int(state_[7]))
-    # print("Discrete state:  ",discrete_state)
-    # print(type(discrete_state))
     return discrete_state
 
 class QLearning:
@@ -26,9 +22,8 @@ class QLearning:
         self.gamma = gamma
         self.epsilon = epsilon
         self.Q = {}
-        
         for act in range(0,4):
-            for a in range(-2,3):
+            for a in range(-3,4):
                 for b in range(-2,3):
                     for c in range(-2,3):
                         for d in range(-2,3):
@@ -40,57 +35,41 @@ class QLearning:
                                             self.Q.update(temp) 
         print(len(self.Q))
 
-
     def train(self):
-        total = 0
+        average_return = []
+        total = 0.0
         for i in range(10000):
-            
             #Decay epsilon so later on it doesn't search as much
-            self.epsilon = max(self.epsilon * 0.996, 0.02)
-            if i > 10000:
-                self.epsilon = 0.0
-            #Start redering at 10000            
-            if i % 1000 == 0 or i % 1000 == 1 or i % 1000 == 2 or i > 9900:
-                self.env = gym.make("LunarLander-v2", render_mode = "human", continuous=False)
-            else:
-                self.env = gym.make("LunarLander-v2", continuous=False)
+            self.epsilon = max(self.epsilon * 0.995, 0.02)
 
-
-            if i % 100 == 0:
-                print(i, "Last 100 mean score: ", total/100)
-                total = 0
-
-            #Initial state
+            self.env = gym.make("LunarLander-v2", continuous=False)
             S = discretize_state(self.env.reset()[0])
-
-            #Until the env terminates or 60 seconds pass
+            
             for _ in range(1000):
-                
                 self.env.render()
-                # choose action A from state S using epsilon greedy policy#
-
-                #These lines are fine
+                # choose action A from state S using epsilon greedy policy
                 A = self.__epsilon_greedy(S)
                 new_S, reward, terminated, _, _ = self.env.step(A)
                 S_ = discretize_state(new_S)
                 
-                total += reward
-                
-                # update Q values These lines are fine
+                total+=reward
+                # update Q values
                 if not terminated:
                     self.Q[(S, A)] += self.alpha * (reward + self.gamma * self.__greedy(S_) - self.Q[(S, A)])
                 else:
                     self.Q[(S, A)] += self.alpha * (reward - self.Q[(S, A)])
                     break
                 S = S_
-                    
-        print(total)
+            
+            if i%100 == 0:
+                average_return.append(total/100)
+                total = 0.0
 
-    #Greedy Checked and is fine
+        return average_return
+
     def __greedy(self, state):
         return max([self.Q[(state, action)] for action in range(4)])
         
-    #Eps Greedy checked and is fine
     def __epsilon_greedy(self, state):
         if np.random.random() < self.epsilon:
             return np.random.choice(range(4))
