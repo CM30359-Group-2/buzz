1a4851a59a28d4d2e7cb506c3bf81ee040608e74
Max Wood <bridges.wood@gmail.com>
2022-12-02 07:12:41 +0000

    Format keyboard agent

--- keyboard_agent.py
+++ keyboard_agent.py
@@ -9,10 +9,11 @@ import time
 stateDataToDump = []
 
 episodeData = {
-            "strategy": "example", # Fill in whatever the chosen strategy is, if none, put 'N/A'
-            "user": "Cam", # Fill in the user who is controlling this demonstration episode
+    "strategy": "example",  # Fill in whatever the chosen strategy is, if none, put 'N/A'
+    "user": "Cam",  # Fill in the user who is controlling this demonstration episode
             "data": stateDataToDump
-    }
+}
+
 
 def writeFile(jsonFile):
     episodeDataToDump = json.dumps(episodeData, indent=4)
@@ -20,8 +21,8 @@ def writeFile(jsonFile):
 
     jsonFile.close()
 
-    
-jsonFile = open("epNo" + str(time.time()) + ".json", "w")
+
+jsonFile = open("/demos/epNo" + str(time.time()) + ".json", "w")
 
 
 def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
@@ -29,15 +30,14 @@ def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
 
     if terminated == False:
 
-        print ("...")
-        print ("Timestep")
+        print("...")
+        print("Timestep")
 
+        print(type(obs_tp1))
 
-        print (type(obs_tp1))
-    
-        print (obs_tp1)
+        print(obs_tp1)
 
-        print (obs_tp1.tolist())
+        print(obs_tp1.tolist())
 
         stateData = {
             "obs_t": obs_t[0].tolist(),
@@ -48,30 +48,23 @@ def car_callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
             "truncated": truncated,
             "info": info
         }
-        
+
         stateDataToDump.append(stateData)
 
     else:
 
         jsonFile = open("epNo" + str(time.time()) + ".json", "w")
-        
+
         writeFile(jsonFile)
 
         stateDataToDump.clear()
 
 
-
-        
-        
-
 # Wrap env in PlayableGame
 play(gym.make("LunarLander-v2", render_mode="rgb_array", continuous=True), keys_to_action={
-"a": np.array([-1.0, -1.0]),
-"w": np.array([1.0, 0.0]),
-"d": np.array([-1.0, 1.0]),
-"wa": np.array([1.0, -1.0]),
-"wd": np.array([1.0, 1.0]),
-}, noop=np.array([0,0]), callback=car_callback)
-
-
-
+    "a": np.array([-1.0, -1.0]),
+    "w": np.array([1.0, 0.0]),
+    "d": np.array([-1.0, 1.0]),
+    "wa": np.array([1.0, -1.0]),
+    "wd": np.array([1.0, 1.0]),
+}, noop=np.array([0, 0]), callback=car_callback)
