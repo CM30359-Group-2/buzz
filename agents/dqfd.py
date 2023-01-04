9860105f8299b0571ffbdcd72279824226c8c368
Max Wood <bridges.wood@gmail.com>
2023-01-04 21:36:05 +0000

    Add DQFD

--- agents/dqfd.py
+++ agents/dqfd.py
@@ -1,113 +1,159 @@
+import json
 import os
+import random
+from gym import Env
 
 import numpy as np
-from utils import calculate_target_values, select_action_epsilon_greedy, copy_model, train_model
-import json
-from buffer import Transition
-from prioritised_replay_buffer import PrioritisedReplayBuffer
-from q_network import QNetwork
+from agents.agent import Agent
+from buffer import ReplayBuffer
+from keras import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
-def dqfd(env, n_episodes=2000, max_t=3000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, pre_training_updates=40000):
-    script_dir = os.path.dirname(__file__)
-    replay_buffer = PrioritisedReplayBuffer(500000, 64, 0.6)
-    agent = QNetwork(env.observation_space.shape[0], 4)
-    target_agent = QNetwork(
-        env.observation_space.shape[0], 4, copy_model(agent.model))
-    epsilon = eps_start
-    beta = 0.4
-    step_count = 0
+from agents.transition import Transition
 
-    if pre_training_updates > 0:
-        # Load the expert data
+class DQFD(Agent):
+    batch_size = 64
+    memory_size = 1000000
+    seed = 42
+
+    def __init__(self, action_space, state_space, pre_training_epochs=40000, checkpoint=False):
+        Agent.__init__(self, action_space, state_space, ReplayBuffer(self.memory_size, self.batch_size, self.seed))
+        self.pre_training_epochs = pre_training_epochs
+        self.checkpoint = checkpoint
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        self.learning_rate = 0.001
+        self.gamma = 0.99
+        self.max_steps = 2000
+        self.model = self.build_model()
+        
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(150, input_dim=self.state_space, activation='relu'))
+        model.add(Dense(120, activation='relu'))
+        model.add(Dense(self.action_space, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
+        return model
+
+    def act(self, state):
+        # Epsilon greedy
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_space)
+        else:
+            action_values = self.model.predict(state)
+            return np.argmax(action_values[0])
+
+    def replay(self):
+        if len(self.memory) < self.batch_size:
+            return
+
+        mini_batch = self.memory.sample()
+        states = np.array([transition.state for transition in mini_batch])
+        next_states = np.array([transition.next_state for transition in mini_batch])
+        rewards = np.array([transition.reward for transition in mini_batch])
+        dones = np.array([transition.done for transition in mini_batch])
+        actions = np.array([transition.action for transition in mini_batch])
+
+        # print(states)
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+
+        target_q_values = rewards + self.gamma * np.amax(self.model.predict_on_batch(next_states), axis=1) * (1 - dones)
+        q_values = self.model.predict_on_batch(states)
+        indices = np.array([i for i in range(self.batch_size)])
+        q_values[[indices], [actions]] = target_q_values
+
+        self.model.fit(states, q_values, epochs=1, verbose=0)
+
+    def pre_train(self):
+        script_dir = os.path.dirname(__file__)
         print("Loading expert data")
-        # For all json files in demo directory
-        transition_total = 0
+        total_transitions = 0
+        
+        demos_path = os.path.join(script_dir, '../demos')
+        for file in os.listdir(demos_path):
+            with open(os.path.join(demos_path, file)) as demo_file:
+                print(file)
 
-        rel_path = "../demos"
-        abs_file_path = os.path.join(script_dir, rel_path)
-        print(abs_file_path)
-
-        for file in os.listdir(abs_file_path):
-            # Load the json file
-            with open(os.path.join(abs_file_path, file)) as json_file:
-                print(f"Loading {file}")
-                # For all transitions in the json file
-                demo = json.load(json_file)
+                demo = json.load(demo_file)
                 for transition in demo["data"]:
-                    transition_total += 1
+                    total_transitions += 1
+                    states = np.reshape(np.array(transition["obs_t"], dtype=np.float32), (1,8))
+                    new_states = np.reshape(np.array(transition["obs_tp1"], dtype=np.float32), (1,8))
 
-                    # Create a state transition from the json data
-                    state_transition = Transition(
-                        transition["obs_t"], transition["action"], transition["rew"], transition["obs_tp1"], transition["terminated"])
-                    # Add the transition to the replay buffer
-                    replay_buffer.add(state_transition)
+                    parsed_transition = Transition(
+                        states, transition["action"], transition["rew"], new_states, transition["terminated"])
+                    if parsed_transition.state.shape != (1,8):
+                        print(parsed_transition.state.shape)
+                    self.remember(parsed_transition)
+            break
+        
+        print(f"Loaded {total_transitions} transitions")
 
-        print(f"Loaded {transition_total} transitions")
-
-        # Pre-train the model
-        print("Pre-training model")
-        for step in range(pre_training_updates):
-            batch = replay_buffer.sample(beta)
-            targets = calculate_target_values(
-                agent, target_agent, batch, 0.99)
-            states = np.array(
-                [state_transition.state for state_transition in batch])
-            train_model(agent.model, states, targets)
-
-            if step % 1000 == 0:
-                # Update the target network
-                print(f"Updating target network after {step} steps")
-                target_agent = QNetwork(
-                    env.observation_space.shape[0], 4, copy_model(agent.model))
+        for _ in range(self.pre_training_epochs):
+            self.replay()
 
         print("Finished pre-training")
-    else:
-        print("Skipping pre-training")
 
-    for episode in range(n_episodes):
-        print(f"Starting episode {episode} with epsilon {epsilon}")
 
-        episode_reward = 0
-        state = env.reset()
+    def train(self, env: Env, episodes=1000):
+        env.reset(seed=self.seed)
+        np.random.seed(self.seed)
+        rewards = []
 
-        for step in range(1, max_t + 1):
-            step_count += 1
-            q_values = agent.get_q_values(state)
-            action = select_action_epsilon_greedy(q_values, epsilon)
-            new_state, reward, done, _ = env.step(action)
+        if self.pre_training_epochs > 0:
+            print("Pre-training")
+            self.pre_train()
+        else:
+            print("Skipping pre-training")
 
-            episode_reward += reward
+        for episode in range(episodes):
+            print(f"Starting episode {episode} with epsilon {self.epsilon}")
 
-            if step == max_t:
-                done = True
+            episode_reward = 0
+            state = env.reset()
+            state = np.reshape(state, (1,8))
 
-            state_transition = Transition(
-                state, action, reward, new_state, done)
-            replay_buffer.add(state_transition)
+            for step in range(1, self.max_steps + 1):
+                action = self.act(state)
+                new_state, reward, done, _ = env.step(action)
+                new_state = np.reshape(new_state, (1,8))
 
-            state = new_state
+                episode_reward += reward
 
-            if step_count % 1000 == 0:
-                target_agent = QNetwork(
-                    env.observation_space.shape[0], 4, copy_model(agent.model))
+                state_transition = Transition(
+                    state, action, reward, new_state, done)
+                self.remember(state_transition)
 
-            if len(replay_buffer) >= 256:
-                batch = replay_buffer.sample(beta)
-                targets = calculate_target_values(
-                    agent, target_agent, batch, 0.99)
-                states = np.array(
-                    [state_transition.state for state_transition in batch])
-                train_model(agent.model, states, targets)
+                state = new_state
+                self.replay()
 
-            if done:
+                if done:
+                    break
+            rewards.append(episode_reward)
+            print(f"{episode}/{episodes}: {step} steps with reward {episode_reward}")
+
+            running_mean= np.mean(rewards[-100:])
+            if running_mean > 200:
+                print(f"Solved after {episode} episodes with reward {running_mean}")
                 break
+            
+            print(f"Average over last 100 episodes: {running_mean}")
+            if episode != 0 and episode % 50 == 0 and self.checkpoint:
+                self.save_model(episode)         
 
-        print(
-            f"Episode {episode} finished after {step} steps with reward {episode_reward}")
-        if episode != 0 and episode % 50 == 0:
-            backup_file = f"dqfd_{episode}.h5"
-            print(f"Backing up model to {backup_file}")
-            agent.model.save(os.path.join(script_dir, backup_file))
+            self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+        return rewards
+               
 
-        # Linear annealing
-        epsilon = max(epsilon * eps_decay, eps_end)
+    def save_model(self, episode: int):
+        script_dir = os.path.dirname(__file__)
+        backup_file = f"dqn{episode}.h5"
+        print(f"Backing up model to {backup_file}")
+        self.model.save(os.path.join(script_dir, backup_file))
+
+
+    def load(self):
+        pass
