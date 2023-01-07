4b40d1b5fb663928f7430c81dd63558e2f304154
Max Wood <bridges.wood@gmail.com>
2023-01-07 17:31:09 +0000

    Move demo and plot scripts

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,4 +1,7 @@
+# Generate demo episodes.
+# Usage: python demo.py <episodes> <checkpoint_path>
 import os
+import sys
 import gym
 import json
 import time
@@ -7,8 +10,8 @@ from agents.q_learning import QLearning
 
 script_dir = os.path.dirname(__file__)
 transitions = []
-filename = os.path.join(script_dir, "demos", "epNo" + str(time.time()) + ".json")
 
+filename = os.path.join(script_dir, "..demos", "epNo" + str(time.time()) + ".json")
 
 def game_callback(obs_t, obs_tp1, action, rew, done, info):
     global filename
@@ -29,7 +32,6 @@ def game_callback(obs_t, obs_tp1, action, rew, done, info):
     if done and len(transitions) > 0 and episode_reward > 200:
         with open(filename, "w") as file:
             dumped_transitions = json.dumps({
-                "strategy": "example",  # Fill in whatever the chosen strategy is, if none, put 'N/A'
                 "user": "Q",  # Fill in the user who is controlling this demonstration episode
                 "data": transitions
             }, indent=4)
@@ -38,7 +40,16 @@ def game_callback(obs_t, obs_tp1, action, rew, done, info):
         filename = os.path.join(script_dir, "demos", "epNo" + str(time.time()) + ".json")
 
 if __name__ == '__main__':
+    if not os.path.exists(os.path.join(script_dir, "..demos")):
+        try:
+            os.mkdir(os.path.join(script_dir, "..demos"))
+        except OSError as e:
+            print(e)
+
+    episodes = int(sys.argv[1])
+    checkpoint_path = sys.argv[2]
+
     env = gym.make('LunarLander-v2')
     agent = QLearning(action_space=env.action_space.n, state_space=env.observation_space.shape[0])
-    agent.play(env, episodes=100, step_callback=game_callback)
+    agent.play(env, episodes, checkpoint_path, render=False, step_callback=game_callback)
 
